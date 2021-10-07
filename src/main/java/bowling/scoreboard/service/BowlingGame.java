1847af26de0c8ab2163ce85802d12b6c48f31a2f
Moachichae <rhdiddl44320@gmail.com>
2021-10-07 21:40:18 +0900

    TEST

--- src/main/java/bowling/scoreboard/service/BowlingGame.java
+++ src/main/java/bowling/scoreboard/service/BowlingGame.java
@@ -1,10 +1,9 @@
 package bowling.scoreboard.service;
 
-<<<<<<< HEAD
-=======
+
 import bowling.scoreboard.dto.Frame;
 import bowling.scoreboard.dto.Pin;
->>>>>>> a55b64b (DTO : Pin,Frame,Player 생성)
+
 import bowling.scoreboard.dto.Player;
 import org.springframework.stereotype.Service;
 
@@ -23,27 +22,18 @@ public class BowlingGame {
           }
      }
 
-<<<<<<< HEAD
-     public List<Player> getPlayers() {
+    public List<Player> getPlayers() {
           return players;
      }
 
      public int getRound() {
           return round;
      }
-
-     public void setRound(int round) {
-          this.round = round;
-     }
-=======
-     public List<Player> getPlayers(){
-          return players;
-     }
+     
 
      public void setPlayerScore(Frame frame){
           // 점수 계산 클래스에 보낸 후
           // 플레이어에게 입력
      }
 
->>>>>>> a55b64b (DTO : Pin,Frame,Player 생성)
 }
---
+++ src/test/java/bowling/scoreboard/view/viewTest.java
@@ -0,0 +1,20 @@
+package bowling.scoreboard.view;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class viewTest {
+
+    @Test
+    public void playViewTest(){
+        //given
+
+        //when
+        // then
+
+    }
+
+}
